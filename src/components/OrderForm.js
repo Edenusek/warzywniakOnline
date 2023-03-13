3ead2c4db0f3df5100c6d81a43e16f6c7b83b1ab
edenus <edenus27@gmail.com>
2023-03-13 20:14:04 +0100

    change fonts and bgc,i still do basket

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-
+import BtnBox from './BtnBox'
 import classes from "./OrderForm.module.css";
 
 const OrderForm = () => {
@@ -8,52 +8,57 @@ const OrderForm = () => {
       <form>
         <div className={classes.wrapper}>
           <div className={classes.formBox}>
-            <h2>Adres zamawiającego</h2>
+            <h3>Adres zamawiającego</h3>
             <div className={classes.inputName}>
               <div>
-                <label htmlFor="name">Imię: </label>
-                <input type="text"  id="name" />
+                <label htmlFor="name">Imię<span className={classes.redStar} >*</span> </label>
+                <input type="text" id="name" />
               </div>
               <div>
-                <label htmlFor="surname" >Nazwisko: </label>
+                <label htmlFor="surname">Nazwisko<span className={classes.redStar} >*</span> </label>
                 <input type="text" id="surname" />
               </div>
             </div>
             <div className={classes.inputOrder}>
-              <label htmlFor="company" >Firma</label>
+              <label htmlFor="company">Firma</label>
               <input type="text" id="company" />
-              <label htmlFor="email" >E-mail</label>
-              <input type="email" id="email" />
-              <label htmlFor="phone">Telefon</label>
-              <input type="number" id="phone" />
-              <label htmlFor="street" >Ulica</label>
-              <input type="text" id="street" />
-              <label htmlFor="houseNumber" >Nr domu/mieszkanie</label>
-              <input type="number" id="houseNumber" />
-              <label htmlFor="city" >Miasto</label>
-              <input type="text" id="city" />
-              <label htmlFor="country" >Kraj</label>
-              <input type="text" id="country" />
-              <label htmlFor="postCode" >Kod pocztowy</label>
-              <input type="number" id="postCode" />
-              <label htmlFor="nip" >NIP</label>
+              <label htmlFor="nip">NIP</label>
               <input type="number" id="nip" />
+              <label htmlFor="email">E-mail<span className={classes.redStar} >*</span></label>
+              <input type="email" id="email" />
+              <label htmlFor="phone">Telefon<span className={classes.redStar} >*</span></label>
+              <input type="number" id="phone" />
+              <label htmlFor="street">Ulica<span className={classes.redStar} >*</span></label>
+              <input type="text" id="street" />
+              <label htmlFor="houseNumber">Nr domu/mieszkanie<span className={classes.redStar} >*</span></label>
+              <input type="number" id="houseNumber" />
+              <label htmlFor="city">Miasto<span className={classes.redStar} >*</span></label>
+              <input type="text" id="city" />
+              <label htmlFor="country">Kraj<span className={classes.redStar} >*</span></label>
+              <input type="text" id="country" />
+              <label htmlFor="postCode">Kod pocztowy<span className={classes.redStar} >*</span></label>
+              <input type="number" id="postCode" />
             </div>
+      <div className={classes.btn} ><BtnBox btn='zamów' /></div>
           </div>
 
-          <div>
-            <div className={classes.basketBox}>
-              <h2>podsumowanie</h2>
+          <div className={classes.basketBox}>
+            <div>
+              <h3>podsumowanie</h3>
               <div className={classes.basket}>
-                <p>2 produkty</p> <p>20 zł</p>
+                <p className={classes.prod}>2 produkty</p> <p>20 zł</p>
               </div>
-              <label>Bon rabatowy</label>
+              <label>Kod rabatowy</label>
               <input type="text" />
             </div>
-            <div  >
-              <h2>produkty w koszyku</h2>
+            <div>
+              <h3>produkty w koszyku</h3>
             </div>
-            <span>Box sport </span> <span>1 szt</span> <span>150 zł</span>
+            <div className={classes.basket}>
+              <span className={classes.boxName}>Box premium </span>{" "}
+              <span className={classes.boxAmount}>1 szt</span>{" "}
+              <span>110 zł</span>
+            </div>
           </div>
         </div>
       </form>
